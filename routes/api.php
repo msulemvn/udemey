21d044557cc88e82f19c5361a22f210d3501cd1b
Muhammad Suleman <msulemvn@gmail.com>
2024-10-10 02:33:11 +0500

    moved 2fa route to auth.php

--- routes/api.php
+++ routes/api.php
@@ -56,7 +56,6 @@ Route::controller(CourseCategoryController::class)->group(function () {
 Route::middleware('auth:api')->group(function () {
     Route::controller(UserController::class)->group(function () {
         Route::get('/profile', 'showUser');
-        Route::post('/2fa', 'Auth\TwoFactorController@verify')->name('2fa.verify');
     });
 
     /*
