fdbea9574d24e3182a35b89c9b90fefece34a8a2
Muhammad Suleman <msulemvn@gmail.com>
2024-10-10 02:21:02 +0500

    added 2fa route in api.php, added authenticated method to AuthController and implemented TwoFactorController

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use PragmaRX\Google2FA\Google2FA;
+
 use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -46,5 +48,27 @@ class AuthController extends Controller
     {
         $token = Auth::refresh();
         return ApiResponse::success(data: ['access_token' => $token]);
+    }
+
+    public function authenticated(Request $request, $user)
+    {
+        if ($user->uses_two_factor_auth) {
+            $google2fa = new Google2FA();
+
+            if ($request->session()->has('2fa_passed')) {
+                $request->session()->forget('2fa_passed');
+            }
+
+            $request->session()->put('2fa:user:id', $user->id);
+            $request->session()->put('2fa:auth:attempt', true);
+            $request->session()->put('2fa:auth:remember', $request->has('remember'));
+
+            $otp_secret = $user->google2fa_secret;
+            $one_time_password = $google2fa->getCurrentOtp($otp_secret);
+
+            return redirect()->route('2fa')->with('one_time_password', $one_time_password);
+        }
+
+        return redirect()->intended($this->redirectPath());
     }
 }
