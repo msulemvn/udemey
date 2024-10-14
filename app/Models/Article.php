2fbea0177d718eeb303c61cebca9ca6da296c77c
Muhammad Suleman <msulemvn@gmail.com>
2024-10-14 12:51:49 +0500

    edited Article model and included import

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Comment;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Article extends Model
 {
