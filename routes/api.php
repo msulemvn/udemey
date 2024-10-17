2d9dc83ab73ded210d2cbc8e84bf07ce4d61f472
iRaziaMunir <Razi.123>
2024-10-17 14:02:30 +0500

    request method is changed for updating site setting

--- routes/api.php
+++ routes/api.php
@@ -149,7 +149,7 @@ Route::middleware('auth:api')->group(function () {
 
     Route::controller(SiteSettingController::class)->group(function () {
         Route::post('/create-site-setting', 'createSetting');
-        Route::put('/update-site-setting/{id}', 'updateSetting');
+        Route::post('/update-site-setting/{id}', 'updateSetting');
         Route::delete('/delete-site-setting/{id}', 'deleteSetting');
         Route::post('/restore-site-setting/{id}', 'restoreSoftDeletedSetting');
         Route::get('/get-site-settings/{id}', 'getSettings');
