7a522635868ae98aebe7bbd72fb6b9ed9c29b7aa
Muhammad Suleman <msulemvn@gmail.com>
2024-10-18 14:21:04 +0500

    updated CommentController and CommentService

--- app/Services/Comment/CommentService.php
+++ app/Services/Comment/CommentService.php
@@ -2,9 +2,74 @@
 
 namespace App\Services\Comment;
 
+use App\Models\Comment;
 use App\Interfaces\Comment\CommentServiceInterface;
 
 class CommentService implements CommentServiceInterface
 {
+/**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCommentRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store($request)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCommentRequest  $request
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function update($request, Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Approve the specified comment.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function approve(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Approve the specified comment.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function disapprove(Comment $comment)
+    {
+        //
+    }
 }
