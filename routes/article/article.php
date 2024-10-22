058c02656bc15f0f199a6151fc1fcc0a6f7133cd
Muhammad Suleman <msulemvn@gmail.com>
2024-10-22 16:12:10 +0500

    added roles back, in routes

--- routes/article/article.php
+++ routes/article/article.php
@@ -4,11 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Article\ArticleController;
 
 Route::middleware('auth:api')->controller(ArticleController::class)->group(function () {
-    Route::middleware('role:student')->group(function () {
+    Route::middleware('role:admin')->group(function () {
         Route::get('article/{id}', 'show');
         Route::get('/articles/slug/{slug}',  'showBySlug');
+        Route::get('/articles',  'index');
     });
-    Route::middleware('role:admin')->group(function () {
+    Route::middleware('role:student')->group(function () {
         Route::post('/create-article',  'store');
         Route::put('/update-article/{id}',  'update');
         Route::delete('/delete-article/{id}', 'destroy');
