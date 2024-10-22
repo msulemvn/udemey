82e4e9dbcd721a0cfbfc7750865dc4f50ee9d9ab
Muhammad Suleman <msulemvn@gmail.com>
2024-10-22 16:12:10 +0500

    added roles back, in routes

--- app/Http/Middleware/Role.php
+++ app/Http/Middleware/Role.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use App\Helpers\ApiResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class Role
@@ -13,19 +14,18 @@ class Role
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return mixed
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle($request, Closure $next, ...$roles)
+    public function handle(Request $request, Closure $next, $role = null)
     {
-        if ($roles) {
-            $user = Auth::user();
-            /** @var \App\Models\User|null $user */
-            if (Auth::check() && $user->hasAnyRole($roles)) {
+        if ($role) {
+            /** @var \App\User|null $user */
+            $user =  Auth::user();
+            if (Auth::check() && $user->hasAnyRole($role)) {
                 return $next($request);
             }
             return ApiResponse::success(message: 'Unauthorized');
-        } else {
-            return $next($request);
         }
+        return $next($request);
     }
 }
