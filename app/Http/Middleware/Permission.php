2131c76a9aa4eefb096c7fe67880b7f86429626c
Muhammad Suleman <msulemvn@gmail.com>
2024-10-23 16:13:43 +0500

    final commit

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -7,7 +7,6 @@ use App\Helpers\ApiResponse;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 
@@ -35,8 +34,8 @@ class Permission
             $permissions = $user->getAllPermissions()->pluck('name')->toArray();
             $availablePermissions = PermissionVariable::allRoutes();
             foreach ($availablePermissions as $permission) {
-                $path = array_key_exists('prefix', $permission) ? $permission['prefix'] . $permission['path'] : $permission['path'];
-                $path = array_key_exists('postfix', $permission) ? $path . $permission['postfix'] : $path;
+                $path = array_key_exists('prefix', $permission) ? $permission['prefix'] . '/' . $permission['path'] : $permission['path'];
+                $path = array_key_exists('postfix', $permission) ? $path . '/' . $permission['postfix'] : $path;
                 if (fnmatch($path, $request->path()) && in_array($permission['permission'], $permissions)) {
                     return $next($request);
                 }
