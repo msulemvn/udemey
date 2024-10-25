84c77fd37f5a4f7fa5d2683bd4960f080724e5cc
iRaziaMunir <Razi.123>
2024-10-25 13:41:33 +0500

    menu item model updated

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -2,18 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Menu;
+use App\Models\Page;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class MenuItem extends Model
 {
     use HasFactory;
     protected $table = 'menu_items';
-    protected $fillable = [
+    protected $guarded = [];
 
-       'name',
-       'page_id',
-       'article_category_id',
-       'position'
-    ];
+
+    public function menu()
+    {
+        return $this->belongsTo(Menu::class);
+    }
+
+    public function pages()
+    {
+        return $this->hasMany(Page::class);
+    }
 }
