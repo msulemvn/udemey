e0d0a34f98671d7796854ef53d599768a7e56e1e
iRaziaMunir <Razi.123>
2024-10-25 17:17:40 +0500

    menu item interface updated

--- app/Interfaces/MenuItemInterface.php
+++ app/Interfaces/MenuItemInterface.php
@@ -1,11 +1,16 @@
 <?php
 namespace App\Interfaces;
 
-interface MenuInterface 
+interface MenuItemInterface 
+
 {
     public function getAll();
+
     public function getById($id);
-    public function create($request);
-    public function update( $request, $id);
+
+    public function create($data);
+
+    public function update($data, $id);
+
     public function delete($id);
 }
