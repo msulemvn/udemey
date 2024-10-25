eaa6da6a01f90f269056a62fd97a9b205bcdef26
iRaziaMunir <Razi.123>
2024-10-25 17:15:14 +0500

    Menu routes  update

--- routes/menu/menu.php
+++ routes/menu/menu.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Menu\MenuController;
 
+// Route::middleware(['auth:api', 'role:admin'])->group(function () {
 Route::controller(MenuController::class)->group(function () {
-Route::get('/get-menus',  'index');
-Route::post('/create-menu',  'store');
-Route::post('/update-menu/{id}',  'update');
-Route::delete('delete-menu/{id}',  'destroy');
+  Route::get('/get-menus',  'index');
+  Route::post('/create-menu',  'store');
+  Route::post('/update-menu/{id}',  'update');
+  Route::delete('delete-menu/{id}',  'destroy');
 });
+// });
