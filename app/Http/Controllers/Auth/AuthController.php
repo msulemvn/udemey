7b9edc4887dd9cbdaa031920e1e323f9d8bcb852
Muhammad Suleman <msulemvn@gmail.com>
2024-10-25 16:44:41 +0500

    registered logout activity

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Helpers\ApiResponse;
+use Illuminate\Http\Request;
+use App\Services\Auth\AuthService;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Traits\HasRoles;
-use App\Services\Auth\AuthService;
 use App\Http\Requests\Auth\LoginAuthRequest;
 use Spatie\Permission\Traits\HasPermissions;
 
@@ -26,9 +27,10 @@ class AuthController extends Controller
         return ApiResponse::success(message: $response['message'] ?? null, data: $response['data'] ?? [], errors: $response['errors'] ?? [], statusCode: $response['statusCode'] ?? 200);
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
         Auth::logout();
+        ApiResponse::activity(request: $request, description: 'User has successfully logged out', showable: true);
         return ApiResponse::success(message: 'Successfully logged out');
     }
 
