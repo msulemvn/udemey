ee5b7d0d1cf5b22b2c30d84adf6607a19a5fc2b1
iRaziaMunir <Razi.123>
2024-10-25 17:11:46 +0500

    Menu item DTO update

--- app/DTOs/MenuItem/MenuItemDTO.php
+++ app/DTOs/MenuItem/MenuItemDTO.php
@@ -2,20 +2,22 @@
 
 namespace App\DTOs\MenuItem;
 
-class MenuItemDTO
+use App\DTOs\BaseDTO;
+
+class MenuItemDTO extends BaseDTO
 {
-  public string $title;
-  public int $order;
-  public bool $is_active;
-  public int $menu_id;
+  public ?string $title;
+  public ?int $order;
+  public ?bool $is_active;
+  public ?int $menu_id;
   public ?int $page_id;
 
   public function __construct($data)
  {
-   $this->title = $data['title'];
-   $this->order = $data['order'];
-   $this->is_active = $data['is_active'];
-   $this->menu_id = $data['menu_id'];
-   $this->page_id = $data['page_id'];
+   $this->title = $data['title'] ?? null;
+   $this->order = $data['order'] ?? null;
+   $this->is_active = $data['is_active'] ?? null;
+   $this->menu_id = $data['menu_id'] ?? null;
+   $this->page_id = $data['page_id'] ?? null;
  }
 }
